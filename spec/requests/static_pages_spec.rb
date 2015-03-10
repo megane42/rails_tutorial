e06f7cc1f744f5ed4250fac277563f937fd9072d
megane42 <h.kzm.gm@gmail.com>
2015-03-10 23:08:22 +0900

    Refactoring the test codes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,28 +4,56 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
+  shared_examples_for 'all_static_pages' do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home Page" do
     before {visit root_path}
-    it { should have_content('Sample App') }
-    it { should have_title("#{base_title}") }
+    let(:heading) { "Sample App" }
+    let(:page_title) { "" }
+
+    it_should_behave_like 'all_static_pages'
     it { should_not have_title("| Home") }
   end
 
   describe "Help Page" do
     before {visit help_path}
-    it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like 'all_static_pages'
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About') }
-    it {should have_title("#{base_title} | About") }
+    let(:heading) { "About" }
+    let(:page_title) { "About" }
+
+    it_should_behave_like 'all_static_pages'
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    let(:heading) { "Contact" }
+    let(:page_title) { "Contact" }
+
+    it_should_behave_like 'all_static_pages'
   end
+
+  it "should have the right link" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title("About"));
+    click_link "Help"
+    expect(page).to have_title(full_title("Help"));
+    click_link "Contact"
+    expect(page).to have_title(full_title("Contact"));
+    click_link "sample app"
+    expect(page).to have_title(full_title(""));
+    click_link "Sign Up Now!"
+    expect(page).to have_title(full_title("Sign up"));
+  end
+
 end
